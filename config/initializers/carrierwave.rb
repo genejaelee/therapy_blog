3352db1b3ff0ba817d8bc2e9a5bebd5bec9070a5
Gene Lee <genejaelee@gmail.com>
2014-08-15 21:14:37 -0400

    clean up initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,15 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => ENV["S3_KEY"],
     :aws_secret_access_key => ENV["S3_SECRET"]
   }
-
+  
+  # Choose what kind of storage to use for this uploader:
+  if Rails.env.production?
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
+  
+  config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
   config.fog_directory    = ENV["S3_BUCKET_NAME"]
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
