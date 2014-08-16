84ae93cb4378e937420b6b1e0aadaac12a4ace67
Gene Lee <genejaelee@gmail.com>
2014-08-15 21:02:40 -0400

    remove public access

--- config/fog.rb
+++ config/fog.rb
@@ -15,6 +15,5 @@ CarrierWave.configure do |config|
   }
   
   config.fog_directory    = ENV["S3_BUCKET_NAME"]
-  config.fog_public     = false 
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
